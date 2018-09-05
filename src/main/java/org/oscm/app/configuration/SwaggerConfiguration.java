2253729891208537195f316cd8236ff71755c9eb
kowalczyka <arkadiusz.kowalczyk@ts.fujitsu.com>
2018-09-05 10:50:54 +0200

    Added initial swagger configuration

---
+++ src/main/java/org/oscm/app/configuration/SwaggerConfiguration.java
@@ -0,0 +1,25 @@
+package org.oscm.app.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+
+    public static final Set<String> DEFAULT_PRODUCES = new HashSet<>(Arrays.asList("application/json"));
+    public static final Set<String> DEFAULT_CONSUMES = new HashSet<>(Arrays.asList("application/json"));
+
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2).produces(DEFAULT_PRODUCES).consumes(DEFAULT_CONSUMES);
+    }
+
+}
--- src/main/java/org/oscm/app/dto/ConfigurationDTO.java
+++ src/main/java/org/oscm/app/dto/ConfigurationDTO.java
@@ -1,19 +1,25 @@
 package org.oscm.app.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import org.oscm.app.domain.enumeration.Controller;
 import org.oscm.app.validation.ControllerId;
 
 import javax.validation.constraints.NotEmpty;
 
+@ApiModel(value = "Configuration", description = "Simple configuration with organization and controller relation")
 public class ConfigurationDTO {
 
+    @ApiModelProperty(position = 1, notes="Identifier of existing configuration", readOnly = true)
     private long id;
 
     @NotEmpty
     @ControllerId
+    @ApiModelProperty(position = 2, notes = "Id of the APP controller", allowableValues = "ess.aws, ess.openstack, ess.azure, ess.vmware", required = true)
     private String controllerId;
 
     @NotEmpty
+    @ApiModelProperty(position = 3, notes = "Id of existing organization", required = true)
     private String organizationId;
 
     public long getId() {
